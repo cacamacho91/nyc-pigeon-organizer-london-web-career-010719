037ae1007c3b94882856d41bfc57a67a55d5b260
dashing-frame-3501 <cacamacho91@gmail.com>
2018-12-09 16:40:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+
+  data.each do |attribute_key, attribute_data|
+    attribute_data.each do |attribute_name, matching_pigeons|
+      matching_pigeons.each do |pigeon|
+        #check if pigeon is in has, if so mofidy them, else add them then modify
+        if pigeons.keys.include?(pigeon)
+          pigeons[pigeon][attribute_key] = attribute_name
+        else
+          pigeons[pigeon] = {}
+          pigeons[pigeon][attribute_key] = attribute_name
+          binding.pry
+        end
+      end
+    end
+  end
 end
